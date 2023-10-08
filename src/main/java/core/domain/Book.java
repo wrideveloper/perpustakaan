49c551f2b7ceb2bfb8cbe270fe5b185cfd476c8c
Noklent <noklen350@gmail.com>
2023-10-08 22:46:29 +0700

    Fix : change Date dattatype to Localdate

--- src/main/java/core/domain/Book.java
+++ src/main/java/core/domain/Book.java
@@ -1,5 +1,6 @@
 package core.domain;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Book {
@@ -7,16 +8,17 @@ public class Book {
     private String name;
     private String author;
     private String publisher;
-    private Date publishedAt;
+    private LocalDate publishedAt;
     private int totalPage;
 
-    public  Book(String isbn, String name, String author, String publisher, Date publishedAt, int totalPage) {
+    public  Book(String isbn, String name, String author, String publisher, LocalDate localDate, int totalPage) {
         this.isbn = isbn;
         this.name = name;
         this.author = author;
         this.publisher = publisher;
-        this.publishedAt = publishedAt;
+        this.publishedAt = localDate;
         this.totalPage = totalPage;
     }
+
 
 }
