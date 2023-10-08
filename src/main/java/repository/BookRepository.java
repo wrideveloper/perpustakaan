1c174798c5ab63359daefb165c8b6244fd462930
Noklent <noklen350@gmail.com>
2023-10-08 22:47:44 +0700

    fix : dummy data and remove throw exception

--- src/main/java/repository/BookRepository.java
+++ src/main/java/repository/BookRepository.java
@@ -3,8 +3,9 @@ package repository;
 import core.domain.Book;
 import usecase.interfaces.IBookRepository;
 
-import java.text.ParseException;
+import java.sql.Date;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,9 +13,9 @@ import java.util.List;
 public class BookRepository implements IBookRepository {
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
     private final List<Book> books = new ArrayList<Book>();
-    public BookRepository() throws ParseException {
-        books.add(new Book("123","Buku 1","Musashi","PT. Berkah",dateFormat.parse("2020-03-03"),200));
-        books.add(new Book("124","Buku 2","Albert","PT. Berkah",dateFormat.parse("2020-03-03"),230));
+    public BookRepository()   {
+        books.add(new Book("123","Buku 1","Musashi","PT. Berkah",LocalDate.of(2003, 02, 20),200));
+        books.add(new Book("124","Buku 2","Albert","PT. Berkah",LocalDate.of(1909, 03, 20),230));
     }
     public Book createBook() {
         // PR
@@ -22,7 +23,6 @@ public class BookRepository implements IBookRepository {
     }
     @Override
     public List<Book> getBooks(){
-
         return books;
     }
     public Book getUserById(String id) {
